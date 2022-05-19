806134b20a0229c4a7c49dd19893bb6e5637d74b
ZaLeep <lexa35695@gmail.com>
2022-05-19 20:05:31 +0300

    Added DirectionalLight and fixed Camera.

--- Lab1/Lab1/DirectionalLight.cs
+++ Lab1/Lab1/DirectionalLight.cs
@@ -6,19 +6,15 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
-    class Camera
+    class DirectionalLight
     {
         Point position;
         Vector3D direction;
-        int[][] screen;
-        float focusDistance;
 
-        public Camera(Point p, Vector3D v, int sHeight, int sWidth, float f)
+        public DirectionalLight(Point p, Vector3D v)
         {
             this.position = p;
             this.direction = Vector3D.Normalize(v);
-            this.screen = new int[sHeight][sWidth];
-            this.focusDistance = f;
         }
 
         public Point getPosition()
@@ -37,21 +33,6 @@ namespace Lab1
         public void setDirection(Vector3D v)
         {
             this.direction = new Vector3D(v);
-        }
-        public float getPosition()
-        {
-            return this.focusDistance;
-        }
-        public void setPosition(float f)
-        {
-            this.focusDistance = f;
-        }
-
-        public void clearScreen()
-        {
-            for (int i = 0; i < this.screen.Length; i++)
-                for (int j = 0; j < this.screen[0].Length; j++)
-                    this.screen[i][j] = 0;
         }
     }
 }
