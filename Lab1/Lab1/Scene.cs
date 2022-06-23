91299f0ca0496e2d65fb0b4ebb43dcb493a81b2c
Alex Gor <ztalexgor@gmail.com>
2022-06-24 00:25:44 +0300

    added support for multiple light sources

--- Lab1/Lab1/Scene.cs
+++ Lab1/Lab1/Scene.cs
@@ -5,15 +5,20 @@ namespace Lab1
     public class Scene
     {
         private readonly Camera cam;
-        private readonly DirectionalLight light;
+        private readonly List<Light> lights;
         private readonly List<ITraceable> objects;
         private float[] viewValues;
+        private Color[] viewColors;
 
         public Scene(string inputPathName)
         {
-            cam = new Camera(new Point(0, 0, -0.75f), new Vector3D(0, 0, 1), 100, 100, 50);
-            light = new DirectionalLight(new Vector3D(1, 1, 1), 1, Color.FromArgb(255, 255, 255, 255));
+            cam = new Camera(new Point(0, 0, -0.75f), new Vector3D(0, 0, 1), 300, 300, 150);
+            lights = new List<Light>();
+            //lights.Add(new DirectionalLight(new Vector3D(1, 1, 1), 1, Color.Violet));
+            lights.Add(new DirectionalLight(new Vector3D(0, -1, 0), 1, Color.Blue));
+            lights.Add(new DirectionalLight(new Vector3D(0, 1, 0), 1, Color.Yellow));
             viewValues = new float[cam.GetScreenHeight() * cam.GetScreenWidth()];
+            viewColors = new Color[cam.GetScreenHeight() * cam.GetScreenWidth()];
             objects = FileWork.ReadObj(inputPathName).GetObjects();
 
             ClearView();
@@ -23,9 +28,11 @@ namespace Lab1
         public Scene(List<ITraceable> objArr)
         {
             cam = new Camera(new Point(0, 0, 0), new Vector3D(0, 0, 1), 20, 20, 5);
-            light = new DirectionalLight(new Vector3D(0, 1, 0.5f), 1, Color.FromArgb(255, 255, 255));
+            lights = new List<Light>();
+            lights.Add(new DirectionalLight(new Vector3D(0, 1, 0.5f), 1, Color.FromArgb(255, 255, 255)));
             objects = objArr;
             viewValues = new float[cam.GetScreenHeight() * cam.GetScreenWidth()];
+            viewColors = new Color[cam.GetScreenHeight() * cam.GetScreenWidth()];
             ClearView();
         }
 
@@ -55,24 +62,68 @@ namespace Lab1
                     Point? intersectionPoint = RayIntersect(ray, out resObj);
                     if (intersectionPoint is not null)
                     {
-                        Beam lightRay = new(intersectionPoint, -light.GetDirection());
+                        List<Color> colors = new List<Color>();
+                        List<float> illuminances = new List<float>();
 
-                        if (RayIsIntersect(lightRay, resObj))
+                        foreach (Light light in lights)
                         {
-                            viewValues[i * screenWidth + j] = 0;
+                            if (light.GetType() == typeof(DirectionalLight))
+                            {
+                                DirectionalLight l = (DirectionalLight)light;
+                                Beam lightRay = new(intersectionPoint, -l.GetDirection());
+                                if (RayIsIntersect(lightRay, resObj))
+                                {
+                                    viewValues[i * screenWidth + j] = 0;
+                                }
+                                else
+                                {
+                                    float view = -(resObj.GetNormalAtPoint(intersectionPoint) * l.GetDirection());
+                                    viewValues[i * screenWidth + j] = view >= 0 ? view : 0;
+                                    
+                                    colors.Add(l.GetColor());
+                                    illuminances.Add(l.GetIntensity() * viewValues[i * screenWidth + j]);
+                                }
+                            }
+                            else if(light.GetType() == typeof(PointLight))
+                            {
+                                PointLight l = (PointLight)light;
+                                if (SegmentIsIntersect(intersectionPoint, l.GetPosition(), resObj))
+                                {
+                                    viewValues[i * screenWidth + j] = 0;
+                                }
+                                else
+                                {
+                                    Vector3D dist = new(intersectionPoint, l.GetPosition());
+                                    float view = -(resObj.GetNormalAtPoint(intersectionPoint) * dist);
+                                    viewValues[i * screenWidth + j] = view >= 0 ? view : 0;
+
+                                    colors.Add(l.GetColor());
+                                    illuminances.Add(l.GetIntensity() / dist.Length() * dist.Length() * viewValues[i * screenWidth + j]);
+                                }
+                            }
+                            else if (light.GetType() == typeof(Light))
+                            {
+                                colors.Add(light.GetColor());
+                                illuminances.Add(light.GetIntensity());
+                            }
+
+                            byte r = 0;
+                            byte g = 0;
+                            byte b = 0;
+                            for (int k = 0; k < colors.Count; k++)
+                            {
+                                r += (byte)Math.Round(illuminances[k] * colors[k].R);
+                                g += (byte)Math.Round(illuminances[k] * colors[k].G);
+                                b += (byte)Math.Round(illuminances[k] * colors[k].B);
+                            }
+
+                            viewColors[i * screenWidth + j] = Color.FromArgb(r, g, b);
                         }
-                        else
-                        {
-                            float view = -(resObj.GetNormalAtPoint(intersectionPoint) * light.GetDirection());
-                            viewValues[i * screenWidth + j] = view >= 0 ? view : 0;
-                            //viewValues[i * screenWidth + j] = view >= 0 ? view : -view;
-                        }
-                        
                     }
                 }
             }
             ViewOutput();
-            FileWork.WritePPM(viewValues, screenHeight, screenWidth, outputPathName);
+            FileWork.WritePPM(viewColors, screenHeight, screenWidth, outputPathName);
         }
 
         public Point RayIntersect(Beam ray, out ITraceable intObj)
@@ -84,16 +135,10 @@ namespace Lab1
             {
                 if(obj is not null) {
                     Point? intersectionPoint = obj.GetIntersectionPoint(ray);
-                    if (intersectionPoint is not null)
-                    {
-                        Vector3D objNormal = obj.GetNormalAtPoint(intersectionPoint);
-                        float dotProductValue = -(objNormal * light.GetDirection());
-                        if (intersectionPoint.Z() < depth)
-                        {
-                            depth = intersectionPoint.Z();
-                            result = intersectionPoint;
-                            intObj = obj;
-                        }
+                    if (intersectionPoint is not null && intersectionPoint.Z() < depth){
+                        depth = intersectionPoint.Z();
+                        result = intersectionPoint;
+                        intObj = obj;
                     }
                 }
             }
@@ -115,7 +160,26 @@ namespace Lab1
             }
             return false;
         }
+        public bool SegmentIsIntersect(Point start, Point end, ITraceable thisObj)
+        {
+            Beam lightRay = new(start, new Vector3D(start, end));
+            float sqDist = new Vector3D(start, end).SquareLength();
 
+            foreach (ITraceable obj in objects)
+            {
+                if (obj is not null && obj != thisObj)
+                {
+                    Point? intersectionPoint = obj.GetIntersectionPoint(lightRay);
+
+                    
+                    if (intersectionPoint is not null && (new Vector3D(start, intersectionPoint)).SquareLength() < sqDist)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
         private void ViewOutput()
         {
             for (int i = 0; i < cam.GetScreenWidth(); i++)
