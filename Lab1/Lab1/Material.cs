8faa2241b9ab79c18f36fc6ce3d03add255ff667
Alex Gor <ztalexgor@gmail.com>
2022-06-28 18:16:34 +0300

    Add realistic reflections

--- Lab1/Lab1/Material.cs
+++ Lab1/Lab1/Material.cs
@@ -4,5 +4,44 @@ namespace Lab1;
 
 public abstract class Material
 {
+    public static Color worldColor = Color.FromArgb(128, 255, 255);
+    protected int height = 0, width = 0;
+    protected Color[]? texture;
     public abstract Color RayBehaviour(Beam startRay, Point interPoint, ITraceable interObj, BVHTree tree, List<Light> lights);
+
+    protected void LoadTexture(string path)
+    {
+        StreamReader? reader = null;
+        try
+        {
+            reader = new(path);
+            reader.ReadLine();
+            string? line = reader.ReadLine();
+
+            string[] splittedLine = line.Split(' ');
+            height = int.Parse(splittedLine[1]);
+            width = int.Parse(splittedLine[2]);
+            
+            reader.ReadLine();
+
+            texture = new Color[height * width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    texture[i * width + j] = Color.FromArgb(reader.Read(), reader.Read(), reader.Read());
+                }
+            }
+        }
+        catch (Exception)
+        {
+            height = 0;
+            width = 0;
+            texture = null;
+        }
+        finally
+        {
+            reader?.Close();
+        }
+    }
 }
--- Lab1/Lab1/Reflective.cs
+++ Lab1/Lab1/Reflective.cs
@@ -4,8 +4,24 @@ namespace Lab1;
 
 public class Reflective : Material
 {
+    private int maxReflectionsNumber;
+    private float reflectionCoeficient;
+
+    public Reflective(int maxReflectionsNumber, float reflectionCoeficient)
+    {
+        this.maxReflectionsNumber = maxReflectionsNumber;
+        this.reflectionCoeficient = reflectionCoeficient;
+    }
+
     public override Color RayBehaviour(Beam ray, Point interPoint, ITraceable interObj, BVHTree tree, List<Light> lights)
     {
+        return RayBehaviour(ray, interPoint, interObj, tree, lights, maxReflectionsNumber);
+    }
+
+    private Color RayBehaviour(Beam ray, Point interPoint, ITraceable interObj, BVHTree tree, List<Light> lights, int numOfReflections)
+    {
+        if (numOfReflections <= 0) return Color.Black;
+
         Vector3D newStartVector = ray.GetDirection() + (interObj.GetNormalAtPoint(interPoint) * 2.0f);
         interPoint += (interObj.GetNormalAtPoint(interPoint) * 0.0001f);
         Beam newStartRay = new(interPoint, newStartVector);
@@ -13,9 +29,12 @@ public class Reflective : Material
         ITraceable? resObject = tree.GetIntersectionObj();
         if (newInterPoint is not null)
         {
-            return resObject.GetColorAtPoint(newStartRay, newInterPoint, tree, lights);
+            Color tempColor = resObject.GetColorAtPoint(newStartRay, newInterPoint, tree, lights);
+            return Color.FromArgb((byte)(tempColor.R * reflectionCoeficient),
+                                  (byte)(tempColor.G * reflectionCoeficient),
+                                  (byte)(tempColor.B * reflectionCoeficient));
         }
 
-        return new Color();
+        return worldColor;
     }
 }
